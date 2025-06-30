2e0023997af4d08eca86b628e7aa58876b4a6441
John Robert Siglos <johnrobert.siglos@gfitftlapt06501.ad.mynt.xyz>
2025-06-30 15:35:40 +0800

    Final Submission, Membership Calculator

---
+++ README.md
@@ -0,0 +1,9 @@
+## John Robert H. Siglos
+# Membership Calculator
+
+Sample Output 1: <br>
+
+![img.png](img.png)
+
+Sample Output 2: <br>
+![img_1.png](img_1.png)
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,50 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
+        final String REGULAR = "Regular";
+        final String VIP = "VIP";
+        final String PREMIUM = "Premium";
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter membership status (Regular, VIP, Premium): ");
+        String membership = scanner.nextLine();
+
+        if (!membership.equalsIgnoreCase(REGULAR) && !membership.equalsIgnoreCase(VIP) && !membership.equalsIgnoreCase(PREMIUM)){
+            System.out.println("Invalid membership status entered.");
+            return;
+        }
+
+        System.out.print("Enter age: ");
+        int age = scanner.nextInt();
+        float price = 0.0f;
+
+        if (membership.equalsIgnoreCase(REGULAR)){
+            if(age < 18){
+                price = 50;
+            }else if(age >= 18 && age <= 64 ){
+                price = 100;
+            }else{
+                price = 75;
+            }
+        } else if (membership.equalsIgnoreCase(VIP)){
+            if(age < 18){
+                price = 75;
+            }else if(age >= 18 && age <= 64 ){
+                price = 150;
+            }else{
+                price = 112.5f;
+            }
+        }else if (membership.equalsIgnoreCase(PREMIUM)){
+            if(age < 18){
+                price = 100;
+            }else if(age >= 18 && age <= 64 ){
+                price = 200;
+            }else{
+                price = 150;
+            }
+        }
+        System.out.print("Price: $");
+        System.out.printf("%.2f", price);
     }
 }
